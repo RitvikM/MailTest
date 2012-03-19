57ad00de701bce0a2bb887567f8685a1e7ccdfb9
Ritvik Menon <menon.ritvik@gmail.com>
2012-03-19 02:13:43 -0400

    intiial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailtest::Application.config.secret_token = 'a3312c5b94ac1c36d8d47f57a4e3cdc619cfd28fdb4b25653b9cce45b98f75c488a1b0226858cd98e9171cf6042462d6caba9863d8272ca678904934bb913dc2'
